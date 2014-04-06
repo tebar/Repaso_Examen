8ae16d03772b7b90eced5a7356eb67d9d15e3438
USUARIO <USUARIO@USUARIO-PC>
2014-04-06 23:11:31 +0200

    subida desde el cliente

--- HolaMUndo/src/Principal.java
+++ HolaMUndo/src/Principal.java
@@ -4,6 +4,7 @@ public class Principal {
 	public static void main(String[] args) {
 
 		System.out.println("Hola Mundo desde mi portatil toshiba!");
+		System.out.println("Hola Mundo desde mi portatil Samsung!");
 
 	}
 
